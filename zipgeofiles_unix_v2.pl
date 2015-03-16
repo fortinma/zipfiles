edd0e5858e76013458424cd324185c920eceffc4
Marcel Fortin <fortingis@gmail.com>
2015-03-16 12:28:41 -0400

    Create zipgeofiles_unix_v2.pl

---
+++ zipgeofiles_unix_v2.pl
@@ -0,0 +1,18 @@
+#!/bin/perl -w
+# Creates a zip file, adding like-named files.
+# Usage: must be executed in the directory where the files are held in UNIX/Linux
+# This version includes handling files with spaces in them.
+
+
+my $extension = ".dwg"; #change to whatever extension you want to use
+
+use strict;
+open(list_of_files, "ls *$extension |");
+my @fileslist = <list_of_files>;
+my $i;
+print "$i\n";
+my $like_file;
+foreach $i (@fileslist) {
+  $like_file = substr($i, 0,-5);
+  system("zip \"$like_file\".zip \"$like_file\".*");
+}
